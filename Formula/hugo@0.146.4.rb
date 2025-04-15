8cf32d92c9fb48273d1005934a2f83c8eb2785ac
Clément Biron <clement.biron@gmail.com>
2025-04-15 20:45:37 +0200

    Add Hugo 0.146.4

---
+++ Formula/hugo@0.146.4.rb
@@ -0,0 +1,37 @@
+class HugoAT01464 < Formula
+  desc "Configurable static site generator"
+  homepage "https://gohugo.io/"
+  url "https://github.com/gohugoio/hugo/archive/refs/tags/v0.146.4.tar.gz"
+  sha256 "0f22fdc8116c0e30f4ea2ba6e2ede2e7d00682cc64f2419dfcb1cfd77966cc88"
+  license "Apache-2.0"
+  head "https://github.com/gohugoio/hugo.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/gohugoio/hugo/common/hugo.commitHash=#{tap.user}
+      -X github.com/gohugoio/hugo/common/hugo.buildDate=#{time.iso8601}
+      -X github.com/gohugoio/hugo/common/hugo.vendorInfo=brew
+    ]
+    tags = %w[extended]
+    system "go", "build", *std_go_args(ldflags:, tags:, output: bin/"hugo")
+
+    generate_completions_from_executable(bin/"hugo", "completion")
+    system bin/"hugo", "gen", "man", "--dir", man1
+  end
+
+  test do
+    site = testpath/"hops-yeast-malt-water"
+    system bin/"hugo", "new", "site", site
+    assert_path_exists site/"hugo.toml"
+
+    assert_match version.to_s, shell_output(bin/"hugo version")
+  end
+end
